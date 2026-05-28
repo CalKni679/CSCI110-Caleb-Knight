74401a60207b26ae8fa0419329a1043dde18a241
CalKni679 <cgknight2@mavs.coloradomesa.edu>
2026-05-27 20:14:26 -0600

    Add files via upload

---
+++ Section 2.14 Problem 1.py
@@ -0,0 +1,18 @@
+#-----------------------|
+#Caleb Knight           |
+#CSCI110                |
+#Section 2.14 Problem 1 |
+#-----------------------|
+
+Shining1 = "All"
+Shining2 = "work"
+Shining3 = "and"
+Shining4 = "no"
+Shining5 = "play"
+Shining6 = "makes"
+Shining7 = "Jack"
+Shining8 = "a"
+Shining9 = "dull"
+Shining10 = "boy."
+
+print (Shining1, Shining2, Shining3, Shining4, Shining5, Shining6, Shining7, Shining8, Shining9, Shining10)
